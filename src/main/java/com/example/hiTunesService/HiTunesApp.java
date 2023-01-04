4ab918970396ac95c373f8c0b60c2da191275927
Wes Keiser <wes@weskeiser.no>
2023-01-04 15:34:10 +0100

    initial commit

---
+++ src/main/java/com/example/hiTunesService/HiTunesApp.java
@@ -0,0 +1,12 @@
+package com.example.hiTunesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiTunesApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HiTunesApp.class, args);
+  }
+}
