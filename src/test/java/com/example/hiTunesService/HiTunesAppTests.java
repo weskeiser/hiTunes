ff6d44bdd5a0bea2c33e1d862da3e577fad20a3b
Wes Keiser <wes@weskeiser.no>
2023-01-06 13:01:21 +0100

    fix: commented out unused test

--- src/test/java/com/example/hiTunesService/HiTunesAppTests.java
+++ src/test/java/com/example/hiTunesService/HiTunesAppTests.java
@@ -1,11 +1,11 @@
-package com.example.hiTunesService;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HiTunesAppTests {
-
-  @Test
-  void contextLoads() {}
-}
+// package com.example.hiTunesService;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class HiTunesAppTests {
+//
+//   @Test
+//   void contextLoads() {}
+// }
